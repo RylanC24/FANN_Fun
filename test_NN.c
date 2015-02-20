4b6251a0843ff8b21b7bda2119e0160004d6419f
Rylan Conway <RylanC24@gmail.com>
2015-02-20 12:48:00 -0800

    saved the testing results to an output file so they can be used for plotting purposes in ipython

--- test_NN.c
+++ test_NN.c
@@ -5,24 +5,18 @@
 int main()
 {
     fann_type *calc_out;
-    int ret = 0;
-
     struct fann *net;
-
     printf("Creating network.\n");
 
-//    char * netFile = "fann220_float_donutSwypeDistance_HLIv4_elliotDeepArch_TanhOut_randWeights_bezTest.net";
-    char * netFile = "Test.net";
+    char * netFile = "./trainedNets/fann_donutSwypeDistance_HLIv4_elliotDeepArch_ElliotOut_randWeights_2kIt_ivL25_5randInterps.net";
+
 #ifdef FIXEDFANN
     net = fann_create_from_file(netFile);
-//    net = fann_create_from_file("fann_float_donutSwypeDistance_HLIv4_elliotDeepArch_ElliotOut_randWeights_2kIt_v2.net");
 #else
     net = fann_create_from_file(netFile);
-//    net = fann_create_from_file("fann_float_donutSwypeDistance_HLIv4_elliotDeepArch_ElliotOut_randWeights_2kIt_v2.net");
 #endif
 
-    if(!net)
-    {
+    if(!net) {
         printf("Error creating ann --- ABORTING.\n");
         return -1;
     }
@@ -31,43 +25,41 @@ int main()
     fann_print_parameters(net);
 
     printf("Loading Testing Data.\n");
-    struct fann_train_data *testData = fann_read_train_from_file("./trainingData/fannTrainSet_RTWP_HLIv4_TanhOut_200k_100k40k_newWL_SloppyBezierCurves_rc3.txt");
+    struct fann_train_data *testData = fann_read_train_from_file("./trainingData/ivL25/fann_trainingSet_RadixHLIv4_ivL25_5interps_1M_350kSame_WLmc20k_RandInterps_2+3.txt");
+    
+    /*Create output file*/
+    FILE *opf;
+    opf = fopen("./fann_HLIv4_elliotDeepArch_ElliotOut_randWeights_wkIt_ivL25_5randInterps_testingOutput.txt","w");
 
-   /*test the network*/
+    /*test the network*/
     printf("Testing the network. %f\n\n", fann_test_data(net, testData));
-
-//    struct fann_train_data *testData = fann_read_train_from_file("./trainingData/fannTrainSet_RTWP_HLIv4_TanhOut_100k_50k20k_newWL_BezierCurves_rc1.txt");
 
     unsigned int i = 0;
     unsigned int total = 0;
     unsigned int correct = 0;
-    unsigned int unsure = 0;
     unsigned int accurate = 0;
 
-//    for(i = 0; i < 500; i++)
-    for(i = 0; i < fann_length_train_data(testData); i++)
-    {
+    for(i = 0; i < fann_length_train_data(testData); i++) {
         calc_out = fann_run(net, testData->input[i]);
-//        printf("Net input: %f %f %f %f %f %f %f %f %f %f %f\n", data->input[i][0],data->input[i][1],data->input[i][2],data->input[i][3],data->input[i][4],data->input[i][5],data->input[i][6],data->input[i][7],data->input[i][8],data->input[i][9],data->input[i][10]);
+        fprintf(opf,"%f %f\n", testData->output[i][0], calc_out[0]);
+
+        //used for debugging neural networks
+        //printf("Net input: %f %f %f %f %f %f %f %f %f %f %f\n", data->input[i][0],data->input[i][1],data->input[i][2],data->input[i][3],data->input[i][4],data->input[i][5],data->input[i][6],data->input[i][7],data->input[i][8],data->input[i][9],data->input[i][10]);
         if(i%10000 == 0)
             printf("Net output: %f intended output: %f\n", calc_out[0], testData->output[i][0]);
 
-        if(testData->output[i][0] == 1)
-        {
-            if(calc_out[0] > 0.5)
-            {
+        if(testData->output[i][0] == 1) {
+            if(calc_out[0] > 0.5) {
                 correct++;
-                if(calc_out[0] > 0.85)
+                if(calc_out[0] > 0.9)
                     accurate++;
             }
         }
 
-        if(testData->output[i][0] == 0)
-        {
-            if(calc_out[0] <= 0.5)
-            {
+        if(testData->output[i][0] == 0) {
+            if(calc_out[0] <= 0.5) {
                 correct++;
-                if(calc_out[0] < 0.15)
+                if(calc_out[0] < 0.1)
                     accurate++;
             }
         }
@@ -75,7 +67,7 @@ int main()
         total++;
     }
 
- 
+    fclose(opf); 
     double eff = (double)correct/(double)total;
     double accEff = (double)accurate/(double)total;
 
